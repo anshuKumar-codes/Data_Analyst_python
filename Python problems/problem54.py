267683a9822c218eb6061982a7c4b277536937f2
Anshu Kumar <anshurajput1828@gmail.com>
2026-02-12 21:56:20 +0530

    update

--- Python problems/problem54.py
+++ Python problems/problem54.py
@@ -1,11 +1,2 @@
-# WAP to to check whether an years is leap year or not.
-
-year=int(input("Enter year: "))
-
-if year%4==0 or year%400==0:
-    print("leap year:",year)
-
-elif year%100==0:
-    print("Not leap year:",year)
 
 

931ec19051d07f5c901234709826bb8238a55455
Anshu Kumar <anshurajput1828@gmail.com>
2026-02-13 16:57:11 +0530

    Delete pandas directory

--- pandas/Readfile.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-# df = pd.read_csv(r"C:\Users\anshu\Downloads\sales_data_sample.csv", encoding="latin1")
-# print(df)
-
-df2 = pd.read_excel(r"C:\Users\anshu\OneDrive\Documents\Excel files\Untitled spreadsheet.xlsx")
-print(df2)
-

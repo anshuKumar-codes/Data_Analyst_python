873b9c3b6b0ea98a00f983d91c6c224236d25bf3
Anshu Kumar <anshurajput1828@gmail.com>
2026-02-17 19:27:39 +0530

    update

---
+++ Python problems/problem57.py
@@ -0,0 +1,16 @@
+# Accept any city from user and print their monument of that city.
+
+city=input("Enter your city: ")
+
+if city=="Delhi" or city=="delhi":
+    print("Monument is : red fort")
+
+elif city=="Agra" or city=="agra":
+    print("Monument is: taj mahael")
+
+elif city=="Jaipur" or city=="jaipur":
+    print("Monument is: Hawa mahal")
+
+else:
+    print("Monument is not found")
+
---
+++ Python problems/problem58.py
@@ -0,0 +1,8 @@
+# WAP to check whether a number entered is three digit number or not
+
+num=int(input("Enter your number: "))
+
+if num>=99:
+    print("This is a three digit number")
+else:
+    print("This is not a three digit number")

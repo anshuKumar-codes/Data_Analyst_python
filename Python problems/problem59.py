5e15abd8ae98c37b69da3958f797f03a0267a601
Anshu Kumar <anshurajput1828@gmail.com>
2026-02-17 19:31:43 +0530

    update

---
+++ Python problems/problem59.py
@@ -0,0 +1,8 @@
+# write a program to check whether a number(accepted from user) is positive or negative.
+
+num=int(input("Enter your number: "))
+
+if num>=0:
+    print("Number is positive")
+else:
+    print("Number is negative")

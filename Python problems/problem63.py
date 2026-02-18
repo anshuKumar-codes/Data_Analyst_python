56c26866cd35bcd98d7fb3ce7343241f2cdfcc59
Anshu Kumar <anshurajput1828@gmail.com>
2026-02-18 21:36:30 +0530

    update

---
+++ Python problems/problem63.py
@@ -0,0 +1,11 @@
+# Accept the following from the user and calculate the percentage of class attended:
+
+# a.Total number of working days
+# b.Total number of days for absent
+
+# After calculating percentage show that, if the percentage is less than 75, than student will not be able to sit in exam.
+
+
+
+
+

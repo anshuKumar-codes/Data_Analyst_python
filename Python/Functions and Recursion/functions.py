6f51ff3bf119a2786718240ebfc514490ea15208
Anshu Kumar <anshurajput1828@gmail.com>
2026-03-09 21:31:37 +0530

    New work

---
+++ Python/Functions and Recursion/functions.py
@@ -0,0 +1,10 @@
+def calc_sum(a,b): # function definition where we define our function and for also take a input(paramter)
+
+    sum=a+b # where we write code that perform by using with the function
+    print(sum)
+
+    return sum # for output 
+
+calc_sum(1,2) # Where we call the function by asign the value of our paramters.when we call the function we take that name when we define with def
+
+

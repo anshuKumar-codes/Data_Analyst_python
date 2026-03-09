c89b156cf8825b635544b093559ed5821d1b07ee
Anshu Kumar <anshurajput1828@gmail.com>
2026-03-09 22:28:37 +0530

    Changes

--- Python/Functions and Recursion/functions.py
+++ Python/Functions and Recursion/functions.py
@@ -1,10 +1,21 @@
-def calc_sum(a,b): # function definition where we define our function and for also take a input(paramter)
+# def calc_sum(a,b): # function definition where we define our function and for also take a input(paramter)
 
-    sum=a+b # where we write code that perform by using with the function
-    print(sum)
+#     sum=a+b # where we write code that perform by using with the function
+#     print(sum)
 
-    return sum # for output 
+#     return sum # for output 
 
-calc_sum(1,2) # Where we call the function by asign the value of our paramters.when we call the function we take that name when we define with def
+# calc_sum(1,2) # Where we call the function by asign the value of our paramters.when we call the function we take that name when we define with def
 
 
+# def printhello(): # without parameter we also create a function and also without return value
+#     print("hello")
+
+# output=printhello()
+# print(output)
+
+def average_numbers(a,b,c):
+    average=(a+b+c)/3
+    print(average)
+    return average
+average_numbers(1,2,3)

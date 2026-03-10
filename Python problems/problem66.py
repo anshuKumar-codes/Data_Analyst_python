ad028e817e3367587f7f486fa51ea782aad666fd
Anshu Kumar <anshurajput1828@gmail.com>
2026-03-10 15:17:46 +0530

    changes

---
+++ Python problems/problem66.py
@@ -0,0 +1,11 @@
+# WAF to find the factorial of n.(n is the parameter)
+
+def print_fact(n):
+    fact=1
+    for i in range(1,n):
+        fact=fact*i
+    print(fact)
+print_fact(6)
+        
+
+    
